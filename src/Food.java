c1c2003b42c345ea8cb91782614ac6b4b4e1814b
NathanWan23 <Nathan.wan23@ycdsbk12.ca>
2022-11-10 10:45:04 -0500

    finished food constructor

--- src/Food.java
+++ src/Food.java
@@ -1,3 +1,12 @@
 public class Food {
     
+    private String name;
+    private double price;
+
+    public Food(String Name, Double Price){
+        name = Name;
+        price = Price;
+    }
+
+    
 }
