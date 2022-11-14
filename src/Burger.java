80b34e8b36f3d1f076d84a489835e729fed80610
NathanWan23 <Nathan.wan23@ycdsbk12.ca>
2022-11-14 10:14:07 -0500

    fixed burger and added to chickenburger

--- src/Burger.java
+++ src/Burger.java
@@ -1,11 +1,17 @@
 public class Burger extends Food {
     
+    private String chickenBurger = "Chicken Burger";
+    private String MorningBurger = "Morning Burger";
+    private String StackedBurger = "Stacked Burger";
+    private String VeganBurger = "Vegan Burger";
+
     
     String[] burgerToppings;
-    public Burger(String Name, String[] toppings){
-        if(Name.equalsTo)
-        super(Name);
+    public Burger(String Name, String[] toppings, double price){
+        super(Name, price);
+        /*super(Name, 5.99);
+        super(Name,10.99);
+        (Name, 8.99);*/
         this.burgerToppings = toppings;
-
     }
 }
---
+++ src/ChickenBurger.java
@@ -0,0 +1,23 @@
+public class ChickenBurger extends Burger {
+    
+    private String chickenBurger = "Chicken Burger";
+    private String MorningBurger = "Morning Burger";
+    private String StackedBurger = "Stacked Burger";
+    private String VeganBurger = "Vegan Burger";
+
+    
+    String[] burgerToppings = {"lettuce", "Tomatoes", "Mayonnaise", "Pickles"};
+    double burgerPrice = 7.99;
+    boolean crisy;
+    int spiceLevel;
+
+    public ChickenBurger(String Name, boolean isCrispy, int spice){
+        super(Name, burgerToppings, burgerPrice);
+        this.crisy = isCrispy;
+        this.spiceLevel = spice; 
+        
+    }
+
+    this.get
+}
+
