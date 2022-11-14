95f52de0d64fc24408e076eed300c793c19e406d
NathanWan23 <Nathan.wan23@ycdsbk12.ca>
2022-11-14 10:30:25 -0500

    fixed chickenburger class

--- src/ChickenBurger.java
+++ src/ChickenBurger.java
@@ -1,18 +1,10 @@
 public class ChickenBurger extends Burger {
     
-    private String chickenBurger = "Chicken Burger";
-    private String MorningBurger = "Morning Burger";
-    private String StackedBurger = "Stacked Burger";
-    private String VeganBurger = "Vegan Burger";
-
-    
-    String[] burgerToppings = {"lettuce", "Tomatoes", "Mayonnaise", "Pickles"};
-    double burgerPrice = 7.99;
     boolean crispy;
     int spiceLevel;
 
     public ChickenBurger(String Name, boolean isCrispy, int spice){
-        super(Name, burgerToppings, burgerPrice);
+        super(Name, new String[] {"lettuce", "Tomatoes", "Mayonnaise", "Pickles"}, 7.99);
         this.crispy = isCrispy;
         this.spiceLevel = spice; 
         
