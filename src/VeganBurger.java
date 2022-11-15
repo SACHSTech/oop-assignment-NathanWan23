053b81202633e514cfd625e6e69d7a50f35cedac
NathanWan23 <Nathan.wan23@ycdsbk12.ca>
2022-11-15 10:03:35 -0500

    lettucebun vegan burger method

--- src/VeganBurger.java
+++ src/VeganBurger.java
@@ -12,5 +12,9 @@ public class VeganBurger extends Burger{
     public boolean getBeyondMeat(){
         return beyondMeat;
     }
+    
+    public boolean getLettuceBun(){
+        return lettuceBun;
+    }
 
 }
