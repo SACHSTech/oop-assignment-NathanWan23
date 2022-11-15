5ff4a503b1ee87f17f224dd756138dd14b99ad95
NathanWan23 <Nathan.wan23@ycdsbk12.ca>
2022-11-15 10:39:38 -0500

    drink constructor

--- src/Drink.java
+++ src/Drink.java
@@ -1,3 +1,12 @@
-public class Drink {
+public class Drink extends Food {
     
+    String size;
+
+    public Drink(String drinkName, String drinkSize){
+        super(drinkName);
+        if(drinkSize == "Small") setPrice(1.99);
+        else if(drinkSize == "Medium") setPrice(2.99);
+        else setPrice(3.99);
+        this.size = drinkSize;
+    }
 }
