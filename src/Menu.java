954edba875d70024d42ce73f5115b674a8e098a4
NathanWan23 <Nathan.wan23@ycdsbk12.ca>
2022-11-15 17:44:48 -0500

    started menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,13 @@
+import java.util.*;
+import java.io.*;
 public class Menu {
+ 
+    ArrayList<Food> foodList = new ArrayList<Food>();
+    Food itemFood;
     
+    public Menu(Food food){
+        itemFood = food;
+        foodList.add(itemFood);
+        
+    }
 }
