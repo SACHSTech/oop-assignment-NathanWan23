5fdc0a1e8078b3cb4fc708f450044f272619ea92
Nathan <nathan.wan23@ycdsbk12.ca>
2022-11-15 18:00:29 -0500

    getSubTotal menu

--- src/Menu.java
+++ src/Menu.java
@@ -5,11 +5,15 @@ public class Menu {
     ArrayList<Food> foodList = new ArrayList<Food>();
     Food itemFood;
     int num = 0;
+    double totalPrice = 0;
+    double subTotal = 0;
+    double tax = 0;
 
     public Menu(Food food){
         itemFood = food;
         foodList.add(itemFood);
         num++;
+        subTotal = subTotal + food.getPrice();
     }
 
     public ArrayList<Food> getfoodList(){
@@ -20,6 +24,8 @@ public class Menu {
         return num;
     }
 
-    public int 
+    public double getSubTotal(){
+        return subTotal;
+    } 
 
 }
