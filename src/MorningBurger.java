be884c7046027cd0fbfbfcd9970c825ae76d8dc9
NathanWan23 <Nathan.wan23@ycdsbk12.ca>
2022-11-15 09:35:04 -0500

    finished morning burger constructor

--- src/MorningBurger.java
+++ src/MorningBurger.java
@@ -1,6 +1,12 @@
 public class MorningBurger extends Burger{
     
-    public MorningBurger(String Name, boolean isCrispy, int spice){
-        super();
+    boolean bagelBun;
+    boolean sausageMeat;
+
+    public MorningBurger(String Name, boolean isBagelBun, boolean isSausageMeat){
+        super(Name, new String[] {"Cheese", "Egg", "Mayonnaise", "Hash Brown", "Bacon"}, 8.99);
+        this.bagelBun = isBagelBun;
+        this.sausageMeat = isSausageMeat; 
     }
+    
 }
