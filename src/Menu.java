6b70e541403a6bbe84f2d7fe726d0aed55369189
Nathan <nathan.wan23@ycdsbk12.ca>
2022-11-15 17:57:59 -0500

    getnumofitem menu

--- src/Menu.java
+++ src/Menu.java
@@ -4,16 +4,20 @@ public class Menu {
  
     ArrayList<Food> foodList = new ArrayList<Food>();
     Food itemFood;
-    
+    int num = 0;
+
     public Menu(Food food){
         itemFood = food;
         foodList.add(itemFood);
-        
+        num++;
     }
 
     public ArrayList<Food> getfoodList(){
         return foodList;
     }
 
+    public int getNumofItem(){
+        return num;
+    }
 
 }
