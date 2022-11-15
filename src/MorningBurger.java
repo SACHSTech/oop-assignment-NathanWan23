d5d04751b7e8586ed3b86d9cf4cd490c7d546f4c
NathanWan23 <Nathan.wan23@ycdsbk12.ca>
2022-11-15 09:59:31 -0500

    started veganburger constructor

--- src/MorningBurger.java
+++ src/MorningBurger.java
@@ -4,7 +4,7 @@ public class MorningBurger extends Burger{
     boolean sausageMeat;
 
     public MorningBurger(String Name, boolean isBagelBun, boolean isSausageMeat){
-        super(Name, new String[] {"Cheese", "Egg", "Mayonnaise", "Hash Brown", "Bacon"}, 8.99);
+        super(Name, new String[] {"Cheese", "Egg", "Mayonnaise", "Hash Brown", "Bacon"}, 5.99);
         this.bagelBun = isBagelBun;
         this.sausageMeat = isSausageMeat; 
     }
--- src/VeganBurger.java
+++ src/VeganBurger.java
@@ -1,3 +1,11 @@
-public class VeganBurger {
+public class VeganBurger extends Burger{
     
+    boolean beyondMeat;
+    boolean lettuceBun;
+
+    public VeganBurger(String Name, boolean isBeyondMeat, boolean isLettuceBun){
+        super(Name, new String[] {"Ketchup", "Mayonnaise", "Lettuce", "Tomatoes", "Grilled onions", "Sauteed mushrooms"}, 7.99);
+        this.bunLayers = BunLayers;
+        this.beefPaties = BeefPaties; 
+    }
 }
