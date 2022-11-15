863413bbfa8dd43f9c4a83d36d427ab29c3ef7b5
NathanWan23 <Nathan.wan23@ycdsbk12.ca>
2022-11-15 10:00:15 -0500

    finished constructor for vegan burger

--- src/VeganBurger.java
+++ src/VeganBurger.java
@@ -5,7 +5,7 @@ public class VeganBurger extends Burger{
 
     public VeganBurger(String Name, boolean isBeyondMeat, boolean isLettuceBun){
         super(Name, new String[] {"Ketchup", "Mayonnaise", "Lettuce", "Tomatoes", "Grilled onions", "Sauteed mushrooms"}, 7.99);
-        this.bunLayers = BunLayers;
-        this.beefPaties = BeefPaties; 
+        this.beyondMeat = isBeyondMeat;
+        this.lettuceBun = isLettuceBun; 
     }
 }
