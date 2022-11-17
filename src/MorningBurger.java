a1b8a2558642ae327e328f3b3c75e409a5c89f62
Nathan <nathan.wan23@ycdsbk12.ca>
2022-11-17 18:06:20 -0500

    contructor javadoc for morning burger

--- src/MorningBurger.java
+++ src/MorningBurger.java
@@ -8,6 +8,13 @@ public class MorningBurger extends Burger{
     boolean bagelBun;
     boolean sausageMeat;
 
+    /**
+     * Constructor - creates a new instance of a burger.
+     * 
+     * @param String name - the name of the chicken burger
+     * @param boolean isCrispy - if the chicken is crispy or grilled
+     * @param int spice - what the spice level will be  
+     */
     public MorningBurger(String Name, boolean isBagelBun, boolean isSausageMeat){
         super(Name, new String[] {"Cheese", "Egg", "Mayonnaise", "Hash brown", "Bacon"}, 5.99);
         this.bagelBun = isBagelBun;
