367c6fb9905c965a2a6c38890f7990e1fb279d82
Nathan <nathan.wan23@ycdsbk12.ca>
2022-11-17 16:58:31 -0500

    Clean up burger class

--- src/Burger.java
+++ src/Burger.java
@@ -1,15 +1,9 @@
 public class Burger extends Food {
     
-    private String chickenBurger = "Chicken Burger";
-    private String MorningBurger = "Morning Burger";
-    private String StackedBurger = "Stacked Burger";
-    private String VeganBurger = "Vegan Burger";
     String[] burgerToppings;
 
     public Burger(String Name, String[] toppings, double price){
         super(Name, price);
-        /*super(Name, 5.99);
-        (Name, 8.99);*/
         this.burgerToppings = toppings;
     }
 
