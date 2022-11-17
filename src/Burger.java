d0e01a39457a6ad5cdf6337eca1e0710b6968e12
Nathan <nathan.wan23@ycdsbk12.ca>
2022-11-17 17:08:25 -0500

    Finished javadoc for burger class

--- src/Burger.java
+++ src/Burger.java
@@ -10,13 +10,20 @@ public class Burger extends Food {
     /**
      * Constructor - creates a new instance of a burger.
      * 
-     * @param name
+     * @param name - the name of the burger
+     * @param toppings - toppings on the burger
+     * @param price - price of the burger
      */
     public Burger(String name, String[] toppings, double price){
         super(name, price);
         this.burgerToppings = toppings;
     }
 
+    /**
+     * Returns array of the burger's toppings.
+     * 
+     * @return String array variable burgerToppings, which represents the burger's toppings
+     */
     public String[] getBurgerToppings(){
         return burgerToppings;
     }
