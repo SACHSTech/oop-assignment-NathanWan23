d7fd555e1b769e56a5115f8f91bcfada48197088
Nathan <nathan.wan23@ycdsbk12.ca>
2022-11-17 20:44:45 -0500

    constructor for vegan burger

--- src/VeganBurger.java
+++ src/VeganBurger.java
@@ -1,10 +1,22 @@
+/**
+ * VeganBurger class file which extends Burger.
+ * @author N. Wan.
+*/
 public class VeganBurger extends Burger{
     
+    // Declare variables
     boolean beyondMeat;
     boolean lettuceBun;
 
-    public VeganBurger(String Name, boolean isBeyondMeat, boolean isLettuceBun){
-        super(Name, new String[] {"Ketchup", "Mayonnaise", "Lettuce", "Tomatoes", "Grilled onions", "Sauteed mushrooms"}, 7.99);
+    /**
+     * Constructor - creates a new instance of a vegan burger.
+     * 
+     * @param String name - the name of the stacked burger burger
+     * @param boolean isBeyondMeat - if the meat is beyond meat
+     * @param boolean isLettuceBun - if the bun is lettuce or poptato bun
+     */
+    public VeganBurger(String name, boolean isBeyondMeat, boolean isLettuceBun){
+        super(name, new String[] {"Ketchup", "Mayonnaise", "Lettuce", "Tomatoes", "Grilled onions", "Sauteed mushrooms"}, 7.99);
         this.beyondMeat = isBeyondMeat;
         this.lettuceBun = isLettuceBun; 
     }
